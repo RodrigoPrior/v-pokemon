38fd919c6bd3ca8b166346af84fedf1dd78952b1
rodrigoprior <rprior@infranology.com.br>
2023-11-02 11:42:14 -0300

    added static swagger

--- src/main.v
+++ src/main.v
@@ -8,12 +8,21 @@ struct App {
 }
 
 fn main() {
-	app := App{}
+	mut app := App{}
+	// app.serve_static('/docs', '.src/assets/docs')
+	app.host_mount_static_folder_at('localhost', './src/assets/docs', '/docs')
 	vweb.run(app, 8080)
 }
 
 pub fn (mut app App) index() vweb.Result {
 	return app.text('Hello world from v!')
+}
+
+['/docs']
+pub fn (mut app App) content() vweb.Result {
+	// call root swagger index
+	content := $tmpl('assets/docs/index.html')
+	return app.html(content)
 }
 
 ['/pokemon/:name'; get]
